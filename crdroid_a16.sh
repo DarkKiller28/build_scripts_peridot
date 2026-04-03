07436c05c97577de2e51908f775e1b4fb4c1d8a5
Soner <oyunbaskani7728@gmail.com>
2026-04-03 17:54:42 +0300

    Create crdroid_a16.sh

---
+++ crdroid_a16.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Build
+brunch peridot
+
+# Copy imgs to a separate folder for easy download
+mkdir -p imgs_output
+cp out/target/product/peridot/boot.img imgs_output/
+cp out/target/product/peridot/init_boot.img imgs_output/
+cp out/target/product/peridot/dtbo.img imgs_output/
+cp out/target/product/peridot/recovery.img imgs_output/
+cp out/target/product/peridot/vendor_boot.img imgs_output/
