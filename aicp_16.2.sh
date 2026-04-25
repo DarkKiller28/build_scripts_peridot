11867dd166fbf3717f8ccf267c5b711fa9a43af4
Soner <oyunbaskani7728@gmail.com>
2026-04-25 16:31:44 +0300

    Create aicp_16.2.sh

---
+++ aicp_16.2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/AICP/platform_manifest.git -b w16.2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+https://github.com/DarkKiller28/local_manifest_peridot.git .repo/local_manifests -b 16.2
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh
+echo "============="
+
+# Remenber
+make clobber
+
+# Build
+time brunch peridot
+
+# Copy imgs to a separate folder for easy download
+mkdir -p imgs_output
+cp out/target/product/peridot/boot.img imgs_output/
+cp out/target/product/peridot/init_boot.img imgs_output/
+cp out/target/product/peridot/dtbo.img imgs_output/
+cp out/target/product/peridot/recovery.img imgs_output/
+cp out/target/product/peridot/vendor_boot.img imgs_output/
