472ab60e177fa479c1deea207380cee435ba2863
Soner <oyunbaskani7728@gmail.com>
2026-07-25 14:10:21 +0300

    scripts: Init --depth=1

--- crdroid_a16.sh
+++ crdroid_a16.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
