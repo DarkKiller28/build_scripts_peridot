604cbe1881e32e2fd4e9c1e61793e08090931c20
Soner <oyunbaskani7728@gmail.com>
2026-09-11 09:42:26 +0300

    scripts: Add fetch build/soong command

--- rising-17.sh
+++ rising-17.sh
@@ -20,6 +20,10 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+# Fetch Build Soong
+cd build/soong && git fetch origin && git reset --hard FETCH_HEAD && cd ../..
+ printf '%s\n' 'com\.peridot' 'com\.peridot\..*' >> build/soong/scripts/check_boot_jars/package_allowed_list.txt
+
 # Export
 export BUILD_USERNAME=DarkKiller
 export BUILD_HOSTNAME=crave
