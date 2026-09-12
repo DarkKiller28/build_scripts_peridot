7fba52019a91a8ad99375a09925c2fe19c50a927
Soner <oyunbaskani7728@gmail.com>
2026-09-12 21:35:49 +0300

    scripts: Create pixelos_16.2.sh

---
+++ pixelos_16.2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/DarkKiller28/local_manifest_peridot.git .repo/local_manifests -b pixelos-16.2 
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+breakfast peridot
+
+# Build
+m pixelos
+
+# Copy imgs to a separate folder for easy download
+mkdir -p imgs_output
+cp out/target/product/peridot/boot.img imgs_output/
+cp out/target/product/peridot/init_boot.img imgs_output/
+cp out/target/product/peridot/dtbo.img imgs_output/
+cp out/target/product/peridot/recovery.img imgs_output/
+cp out/target/product/peridot/vendor_boot.img imgs_output/
