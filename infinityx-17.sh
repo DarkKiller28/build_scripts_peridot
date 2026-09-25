f8102dbdbb50177d035e131b6589d504526dc676
Soner <oyunbaskani7728@gmail.com>
2026-09-25 18:05:37 +0300

    scripts: Create infinityx-17.sh

---
+++ infinityx-17.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 17 -g default,-mips,-darwin,-notdefault --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/DarkKiller28/local_manifest_peridot.git .repo/local_manifests -b infinityx-17
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Fetch Build Soong
+cd build/soong && git remote add custom https://github.com/DarkKiller28/android_build_soong.git && git fetch custom && git reset --hard custom/seventeen && cd ../..
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch infinity_peridot-userdebug
+
+# Build
+mka bacon
+
+# Copy imgs to a separate folder for easy download
+mkdir -p imgs_output
+cp out/target/product/peridot/boot.img imgs_output/
+cp out/target/product/peridot/init_boot.img imgs_output/
+cp out/target/product/peridot/dtbo.img imgs_output/
+cp out/target/product/peridot/recovery.img imgs_output/
+cp out/target/product/peridot/vendor_boot.img imgs_output/
